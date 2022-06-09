5625703ad7c88df50798c6c69558a3c461373242
月知忆 <coderabbite666@gmail.com>
2022-06-09 15:14:43 +0800

    Changes

---
+++ xsgj.js
@@ -0,0 +1,263 @@
+let obj = JSON.parse($response.body);
+obj = {
+    "auto" : [
+        {
+            "ipAddress" : "154.17.18.180",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "199.180.112.36",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "89.208.247.220",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "89.208.242.206",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "212.103.62.7",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "173.242.124.121",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "178.157.63.75",
+            "vip" : "0",
+            "local" : "Tokyo",
+            "nation" : "jp"
+        },
+        {
+            "ipAddress" : "46.101.47.14",
+            "vip" : "0",
+            "local" : "London-英国",
+            "nation" : "uk"
+        },
+        {
+            "ipAddress" : "159.223.18.132",
+            "vip" : "0",
+            "local" : "Frankfurt-德国",
+            "nation" : "ger"
+        },
+        {
+            "ipAddress" : "206.189.1.162",
+            "vip" : "0",
+            "local" : "Amsterdam-荷兰",
+            "nation" : "nether"
+        },
+        {
+            "ipAddress" : "138.197.148.238",
+            "vip" : "0",
+            "local" : "Toronto-加拿大",
+            "nation" : "canada"
+        },
+        {
+            "ipAddress" : "68.183.188.63",
+            "vip" : "0",
+            "local" : "Singapore",
+            "nation" : "sg"
+        },
+        {
+            "ipAddress" : "23.225.64.23",
+            "vip" : "0",
+            "local" : "HongKong",
+            "nation" : "hk"
+        },
+        {
+            "ipAddress" : "161.8.184.62",
+            "vip" : "0",
+            "local" : "05-18-LA161.8.184.62",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "161.8.184.63",
+            "vip" : "0",
+            "local" : "05-18-LA161.8.184.63",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "161.8.184.64",
+            "vip" : "0",
+            "local" : "05-18-LA161.8.184.64",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "161.8.184.65",
+            "vip" : "0",
+            "local" : "05-19-LA161.8.184.65",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "161.8.184.68",
+            "vip" : "0",
+            "local" : "05-19-LA161.8.184.68",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "149.248.17.123",
+            "vip" : "0",
+            "local" : "06-05-LA149.248.17.123",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "149.28.80.84",
+            "vip" : "0",
+            "local" : "06-05-LA149.28.80.84",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "45.32.69.70",
+            "vip" : "0",
+            "local" : "06-05-LA45.32.69.70",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "45.32.90.202",
+            "vip" : "0",
+            "local" : "06-05-LA45.32.90.202",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "149.248.13.169",
+            "vip" : "0",
+            "local" : "06-05-LA149.248.13.169",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "66.42.107.139",
+            "vip" : "0",
+            "local" : "06-05-LA66.42.107.139",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "45.63.51.56",
+            "vip" : "0",
+            "local" : "06-05-LA45.63.51.56",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "108.61.214.175",
+            "vip" : "0",
+            "local" : "06-05-LA108.61.214.175",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "8.3.29.96",
+            "vip" : "0",
+            "local" : "06-05-LA8.3.29.96",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "45.32.93.84",
+            "vip" : "0",
+            "local" : "06-05-LA45.32.93.84",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "45.63.37.198",
+            "vip" : "0",
+            "local" : "06-06-SEA45.63.37.198",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "137.220.39.57",
+            "vip" : "0",
+            "local" : "06-06-SEA137.220.39.57",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "66.42.77.126",
+            "vip" : "0",
+            "local" : "06-06-SEA66.42.77.126",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "137.220.34.42",
+            "vip" : "0",
+            "local" : "06-06-SEA137.220.34.42",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "66.42.70.123",
+            "vip" : "0",
+            "local" : "06-06-SEA66.42.70.123",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "144.202.87.64",
+            "vip" : "0",
+            "local" : "06-06-SEA144.202.87.64",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "45.77.211.177",
+            "vip" : "0",
+            "local" : "06-06-SEA45.77.211.177",
+            "nation" : "usa"
+
+        },
+        {
+            "ipAddress" : "149.28.10.34",
+            "vip" : "0",
+            "local" : "06-06-SEA149.28.10.34",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "66.42.66.221",
+            "vip" : "0",
+            "local" : "06-06-SEA66.42.66.221",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "149.28.9.224",
+            "vip" : "0",
+            "local" : "06-06-SEA149.28.9.224",
+            "nation" : "usa"
+        }
+    ],
+    "user" : [
+        {
+            "ipAddress" : "",
+            "vip" : "0",
+            "local" : "06-05-LA",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "",
+            "vip" : "0",
+            "local" : "04-13-SV",
+            "nation" : "x2"
+        }
+    ],
+    "hand" : [
+        {
+            "ipAddress" : "154.17.18.180",
+            "vip" : "1",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        },
+        {
+            "ipAddress" : "161.8.184.62",
+            "vip" : "0",
+            "local" : "LosAngeles",
+            "nation" : "usa"
+        }
+    ]
+};
+$done({body: JSON.stringify(obj)});
